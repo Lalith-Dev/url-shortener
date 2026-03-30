59ee054522e6d18658e8c3daefd4369ddcfb4de0
Lalith-Dev <lalithepythoneer@gmail.com>
2026-03-30 18:44:46 +0100

    Database changes

--- app/database/db.py
+++ app/database/db.py
@@ -2,12 +2,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = None
+
+# 🔥 Retry connection
+for i in range(10):
+    try:
+        engine = create_engine(DATABASE_URL)
+        connection = engine.connect()
+        connection.close()
+        print("Connected to PostgreSQL ✅")
+        break
+    except Exception as e:
+        print("Waiting for PostgreSQL...")
+        time.sleep(2)
+
+if engine is None:
+    raise Exception("Could not connect to PostgreSQL")
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
